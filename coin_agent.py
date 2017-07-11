4508eddf0074d91ed0a6e3151f7b8cec4d6b5827
fin10 <fender64@gmail.com>
2017-07-11 23:57:20 +0900

    Fix model

--- coin_agent.py
+++ coin_agent.py
@@ -7,8 +7,9 @@ from enum import Enum
 import numpy as np
 import tensorflow as tf
 
+
 # os.environ['CUDA_VISIBLE_DEVICES'] = '1'
-tf.logging.set_verbosity(tf.logging.INFO)
+# tf.logging.set_verbosity(tf.logging.INFO)
 
 
 class CoinAgent:
@@ -16,6 +17,9 @@ class CoinAgent:
         NOTHING = 0
         BUY = 1
         SELL = 2
+
+    actions = list(Action)
+    num_outputs = len(actions)
 
     def __init__(self, params):
         self.epsilon = params['epsilon']
@@ -34,22 +38,19 @@ class CoinAgent:
             },
         )
 
-    def input_fn(self, orders, target=[0.0 for _ in range(len(list(Action)))]):
+    def input_fn(self, orders, target=[0.0 for _ in range(num_outputs)]):
         if self.max_length < len(orders):
             orders = orders[len(orders) - self.max_length:]
 
         data = []
-        first_time = float(orders[0]['timestamp'])
-
         for order in orders:
             price = float(order['price'])
             qty = float(order['qty'])
-            timestamp = float(order['timestamp']) - first_time
-            data.append([price, qty, timestamp])
+            data.append([price, qty])
 
         if self.max_length > len(data):
             for _ in range(self.max_length - len(data)):
-                data.append([0.0, 0.0, 0.0])
+                data.append([0.0, 0.0])
 
         inputs = {
             'state': tf.constant(data)
@@ -64,10 +65,6 @@ class CoinAgent:
         state = features['state']
         hidden_units = params['hidden_units']
         learning_rate = params['learning_rate']
-        num_outputs = len(list(cls.Action))
-
-        state = tf.stack([state])
-        target = tf.stack([target])
 
         outputs = tf.contrib.layers.fully_connected(
             inputs=state,
@@ -78,23 +75,20 @@ class CoinAgent:
 
         q = tf.contrib.layers.fully_connected(
             inputs=outputs,
-            num_outputs=num_outputs,
+            num_outputs=cls.num_outputs,
         )
 
-        loss = tf.losses.softmax_cross_entropy(
-            onehot_labels=target,
-            logits=q,
-        )
+        q = tf.reshape(q, [-1])
+
+        # loss = tf.losses.softmax_cross_entropy(
+        #     onehot_labels=target,
+        #     logits=q,
+        # )
+
+        loss = tf.reduce_sum(tf.square(target - q))
 
         train_op = None
         if mode == tf.contrib.learn.ModeKeys.TRAIN:
-            learning_rate = tf.train.exponential_decay(
-                learning_rate=learning_rate,
-                global_step=tf.contrib.framework.get_global_step(),
-                decay_steps=100,
-                decay_rate=0.96
-            )
-
             train_op = tf.contrib.layers.optimize_loss(
                 loss=loss,
                 global_step=tf.contrib.framework.get_global_step(),
@@ -104,51 +98,75 @@ class CoinAgent:
 
         return tf.contrib.learn.ModelFnOps(
             mode=mode,
-            predictions={
-                'action_dist': q
-            },
+            predictions=q,
             loss=loss,
             train_op=train_op
         )
 
-    def select(self, dataset, step: int):
+    def select(self, state, step: int):
         threshold = min(self.epsilon, step / 100.0)
         if threshold > random.random():
-            prediction = self.estimator.predict(
-                input_fn=lambda: self.input_fn(dataset)
-            )
-
-            prediction = list(prediction)[0]['action_dist']
-            idx = np.argmax(prediction)
-
-            return self.Action(idx)
+            return list(self.estimator.predict(
+                input_fn=lambda: self.input_fn(state)
+            ))
         else:
-            return random.choice(list(self.Action))
+            print('#%d random pick' % step)
+            result = [0 for _ in range(self.num_outputs)]
+            result[random.randint(0, len(result) - 1)] = 1
+            return result
 
-    def update(self, current, next, reward):
-        if not os.path.exists('./model'):
-            self.estimator.fit(
-                input_fn=lambda: self.input_fn(current),
-                steps=1
-            )
-        else:
-            action = self.estimator.predict(
-                input_fn=lambda: self.input_fn(current)
-            )
-            next_action = self.estimator.predict(
-                input_fn=lambda: self.input_fn(next)
-            )
+    def update(self, reward, current_state, current_action_dist, next_action):
+        idx = np.argmax(next_action)
+        current_action_dist[idx] = reward + self.gamma * next_action[idx]
 
-            action = list(action)[0]['action_dist']
-            next_action = list(next_action)[0]['action_dist']
+        self.estimator.fit(
+            input_fn=lambda: self.input_fn(current_state, current_action_dist),
+            steps=1
+        )
 
-            idx = np.argmax(next_action)
-            action[idx] = reward + self.gamma * next_action[idx]
+    def train(self, trade_files):
+        commission = 0.0015
+        budget = 10000
+        num_coins = 0
+        coin_value = 0
 
-            self.estimator.fit(
-                input_fn=lambda: self.input_fn(current, action),
-                steps=1
-            )
+        for idx in range(len(trade_files) - 1):
+            state = trade_files[idx]
+            with open(state) as fp:
+                orders = json.load(fp)['orders']
+
+            portfolio = budget + num_coins * coin_value
+            action_dist = self.select(orders, idx)
+            action = CoinAgent.Action(np.argmax(action_dist))
+
+            coin_value = int(orders[-1]['price'])
+            if action == CoinAgent.Action.BUY and budget > 0:
+                bought = (budget / coin_value) * (1 - commission)
+                budget -= int(bought * coin_value)
+                num_coins += bought
+            elif action == CoinAgent.Action.SELL and num_coins > 0:
+                budget += int(num_coins * coin_value * (1 - commission))
+                num_coins = 0
+
+            new_portfolio = budget + num_coins * coin_value
+            reward = new_portfolio - portfolio
+
+            next_state = trade_files[idx + 1]
+            with open(next_state) as fp:
+                next_orders = json.load(fp)['orders']
+            next_action_dist = self.select(next_orders, idx)
+
+            self.update(reward, orders, action_dist, next_action_dist)
+
+            print('#%d (%s), Portfolio: %f, Budget: %d, Coin: %f(%d)' %
+                  (idx, action, (budget + num_coins * coin_value), budget, num_coins, coin_value))
+
+        return {
+            'portfolio': (budget + num_coins * coin_value),
+            'budget': budget,
+            'num_coins': num_coins,
+            'coin_value': coin_value
+        }
 
 
 if __name__ == '__main__':
@@ -178,41 +196,13 @@ if __name__ == '__main__':
         'max_length': 12000
     })
 
-    commission = 0.0015
-    budget = 10000
-    num_coins = 0
-    coin_value = 0
+    trial = 50
+    results = []
+    for idx in range(trial):
+        result = agent.train(files)
+        results.append(result)
 
-    for idx in range(len(files) - 1):
-        state = files[idx]
-        with open(state) as fp:
-            orders = json.load(fp)['orders']
-
-        portfolio = budget + num_coins * coin_value
-        action = agent.select(orders, idx)
-        coin_value = int(orders[-1]['price'])
-
-        if action == CoinAgent.Action.BUY and budget > 0:
-            bought = (budget / coin_value) * (1 - commission)
-            budget -= int(bought * coin_value)
-            num_coins += bought
-        elif action == CoinAgent.Action.SELL and num_coins > 0:
-            budget += int(num_coins * coin_value * (1 - commission))
-            num_coins = 0
-
-        new_portfolio = budget + num_coins * coin_value
-        reward = new_portfolio - portfolio
-        next_state = files[idx + 1]
-        with open(next_state) as fp:
-            next_orders = json.load(fp)['orders']
-
-        agent.update(orders, next_orders, reward)
-
-        print('#%d (%s), Portfolio: %f, Budget: %d, Coin: %f(%d)' %
-              (idx, action, (budget + num_coins * coin_value), budget, num_coins, coin_value))
-
-    print('# Result')
-    print('# Portfolio: %f' % (budget + num_coins * coin_value))
-    print('# Budget: %d' % budget)
-    print('# Number of coins: %f' % num_coins)
-    print('# Value of a coin: %f' % coin_value)
+    print('- List of Portfolios -')
+    for idx, result in enumerate(results, 1):
+        print('#%d Portfolio: %f, Budget: %d, Coin: %f(%d)' %
+              (idx, result['portfolio'], result['budget'], result['num_coins'], result['coin_value']))
